779d8df9d2187d7844d7e6c53bc650d2e09f6b35
Massachusetts Environmental Public Health Tracking <32905383+ma-dph-beh@users.noreply.github.com>
2025-11-19 10:35:32 -0500

    Tox spanish uploads

---
+++ TOX/climate-change-graphics/Spanish/Climate_Actions-Acciones_climáticas/story_content/triggers.js
@@ -0,0 +1,75 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5fBCTO5JilC":
+        Script1();
+        break;
+      case "65j9ocHPAPo":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5e4w3t7kJ6Z');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Go45vBvXEt';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('6DRRH6X1TR5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6U79ZOKXldo';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
